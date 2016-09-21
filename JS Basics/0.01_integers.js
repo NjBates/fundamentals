1c58051dc9df874c1b0d44d3adf2de33acfa7427
njb93 bates <njb93 bates>
2016-09-21 10:34:01 -0400

    added js basics and js folder

---
+++ JS Basics/0.01_integers.js
@@ -0,0 +1,52 @@
+// var num = 27;
+// var newNum = 10;  // this is camel casing 
+// var newer_num = 3  // this is snake casing
+
+// var API_KEY = 125482538142 // how internet interacts
+
+//console.log() prints to the console
+
+//operations with console.log
+console.log(5 + 5);
+//TODO - make a subtraction, multiplication and division console.log
+
+//modulus operator
+
+// console.log(18%3);
+
+// console.log(20%7);
+
+// var x = 27 + 99;
+
+// var y = 22 + 87;
+
+// var z = x - y;
+
+// console.log(z);
+
+// var newNum = 9;
+
+// newNum = newNum + 3;
+
+// console.log(newNum);
+
+// newNum++; //short cut for newNum = newNum + 1;
+
+// newNum= newNum - 1;
+
+// newNum--
+
+// console.log(newNum);
+
+
+// newNum = newNum + 10;
+
+// console.log(newNum);
+
+// newNum += 10; //short cut for writing newNum = newNum + 10;
+
+var newNum = 20;
+
+newNum -= 10;
+
+console.log(newNum); 
