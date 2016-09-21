cb7914006f0d4e371e28ec0a9c6dfabf940b912a
njb93 bates <njb93 bates>
2016-09-21 12:00:13 -0400

    Booleans

---
+++ JS Basics/0.04_booleans.js
@@ -0,0 +1,44 @@
+//booleans are true/false values and help with
+//control flow and logic
+// var hangry = true;
+// console.log(hangry)
+
+//operators
+// == Equals
+// === Strict Equality "7" === 7;
+// != Inequality / does not equal;
+// !== Strict Inequality
+// > Greater than
+// >= Greater or equal;
+// < less than
+// <= less than or equal
+
+var num = 5 < 2;
+console.log(num)
+
+var newNum = 5 > 2;
+console.log(newNum)
+
+console.log((num == newNum));
+
+var x = 1;
+var y = "1";
+
+console.log(x === y)
+
+var t = 23;
+var u = 23;
+
+var q = 2;
+var w = 4;
+
+var z = 27;
+var x = 8;
+
+var o = 45;
+var p = 12;
+
+console.log(t >= u)
+console.log(q <= w)
+console.log(z != x)
+console.log(o !== p)
